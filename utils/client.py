f9ad745bf1e30bd6a95e84924a3f467a508a2a48
Yasser Sulaiman <YasserSulaiman.is@gmail.com>
2023-12-14 12:19:51 +0100

    moving files to utils folder

---
+++ utils/client.py
@@ -0,0 +1,262 @@
+import socket
+import os
+import pickle
+import subprocess
+import sys
+
+def check_filename_exists(filename, filenames_on_server):
+    if filename not in filenames_on_server:
+        print('File not found, please try again')
+        return False
+    return True
+
+
+def read_file_from_server(file_path, client):
+    filename = file_path.split('/')[-1]
+    file = open(filename, 'wb')#extract filename from path
+    file_bytes = b""
+    while True:
+        data = client.recv(1024)
+        if data[-5:] == b'<End>':
+            break
+        else:
+            file_bytes += data
+    file.write(file_bytes)
+    file.close()
+
+    if sys.platform.startswith('win32'):
+        os.startfile(filename)
+    elif sys.platform.startswith('darwin'):
+        subprocess.run(('open', filename), check=True)
+    elif sys.platform.startswith('linux'):
+        subprocess.run(('xdg-open', filename), check=True)
+
+
+def write_file_to_server(file_path, client):
+    try:
+        filename = file_path.split('/')[-1]
+        file = open(filename, 'rb')
+        data = file.read(1024)
+        while data:
+            client.send(data)
+            data = file.read(1024)
+        client.send(b'<End>')
+        file.close()
+        return True
+    except:
+        print('An error happened, please try again')
+        return False
+
+
+def get_directory_from_path(directories_dict, path):
+    if path == 'root':
+        return directories_dict
+    path_list = path.split('/')
+    path_list = path_list[1:]
+    current_dict = directories_dict
+    for directory in path_list:
+        current_dict = current_dict[directory]
+    return current_dict
+
+
+def prompt_user(directories_dict, current_directory):
+    print('==============================================')
+    print("These are the files currently in the server's "  + current_directory + " directory: ")
+    current_directory_dict = get_directory_from_path(directories_dict, current_directory)
+    print(print_directory_tree(current_directory_dict))
+    print('==============================================')
+    print('What operation would you like to do?')
+    print('For reading a file, write "read" then the file name')
+    print('For writing a file, write "write" then the file name')
+    print('For updating a file, write "update" then the file name')
+    print('For deleting a file, write "delete" then the file name')
+    print('For changing directory, write "cd" then the directory name')
+    print('For exiting, write "exit"')
+    print('Please note that the file name must be the full path from the current directory')
+
+
+def have_more_operations():
+    if input('Do you want to do more operations? (y/n) \n') == 'y':
+        return True
+    return False
+
+
+def print_directory_tree(directory_tree, indent=0):
+    for key, value in directory_tree.items():
+        if value is None:
+            print('  ' * indent + f'- {key} (file)')
+        else:
+            print('  ' * indent + f'- {key} (directory)')
+            print_directory_tree(value, indent + 1)
+
+
+def cd_back_steps(current_directory , path):
+    path_list = path.split('/')
+    for step in path_list:
+        if step == '..':
+            current_directory = '/'.join(current_directory.split('/')[:-1])
+            path_list = path_list[1:]
+    return current_directory, '/'.join(path_list)
+
+
+def is_valid_directory(directories_dict, current_directory, directory_path):
+    #get rid of back steps
+    current_directory, directory_path = cd_back_steps(current_directory, directory_path)
+    if directory_path == '': return True #if user entered only back steps
+    #check that directory exists
+    directory_name_list = list(directory_path.split('/'))
+    for directory in directory_name_list:
+        if directory not in list(get_directory_from_path(directories_dict, current_directory).keys()):
+            print('This directory does not exist, please enter a directory from the list:')
+            return False
+        else:
+            current_directory = current_directory + '/' + directory
+    #check that last item in path is a directory not a filename
+    if get_directory_from_path(directories_dict, current_directory) is None:
+        print('This is a file, please enter a directory from the list:')
+        return False
+    return True
+
+
+def is_valid_path(directories_dict, current_directory, path):
+    #get rid of back steps
+    current_directory, directory_path = cd_back_steps(current_directory, path)
+    if directory_path == '': return True #if user entered only back steps
+    #check that directory/file exists
+    path_list = path.split('/')
+    path_list = path_list
+    for directory in path_list:
+        if directory not in list(get_directory_from_path(directories_dict, current_directory).keys()):
+            return False
+        else:
+            current_directory = current_directory + '/' + directory
+    return True
+
+
+def receive_tree_from_server(client):
+    directories_dict = client.recv(4096)
+    directories_dict = pickle.loads(directories_dict)
+    return directories_dict
+
+
+print('Connecting to the server...')
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(('localhost', 9999))
+
+
+def client_main_app(client):
+    current_directory = 'root'
+    #Once connected, the client receives the directory tree from the server
+    directories_dict = receive_tree_from_server(client)
+    while True:
+        
+
+        while True: #loop until valid input
+            try:
+                # Print the files on the server and the operations that can be done
+                prompt_user(directories_dict, current_directory)
+                operation_filename = input('Please enter your operation: \n')
+                operation_filename_list = operation_filename.split(' ', 1)
+                operation = operation_filename_list[0].lower()
+                if operation == 'exit': break
+                filename = operation_filename_list[1]
+                if operation not in ['read', 'write', 'update', 'delete','cd']:
+                    raise Exception
+                if operation in ['read','update','delete'] and not is_valid_path(directories_dict, current_directory, filename):
+                    print('This file does not exist, please enter a file from the list')
+                    raise Exception
+                if operation == 'write' and not os.path.exists(filename):#check that file exists on client side
+                    raise Exception
+                if operation == 'write' and is_valid_path(directories_dict, current_directory, filename): 
+                    if input('File already exists on the server, do you want to overwrite it? (y/n) \n') != 'y':
+                        continue #if user doesn't want to overwrite, re-prompt user for another operation
+                if operation == 'delete' and input('Are you sure you want to delete this file? (y/n) \n') != 'y':
+                    continue #if user doesn't want to delete, re-prompt user for another operation
+                if operation == 'cd'and not is_valid_directory(directories_dict, current_directory, filename):
+                        raise Exception
+                break #break out of loop if valid input
+            #handle if invalid input
+            except:
+                print('Invalid input, please try again')
+        import sys        
+        if operation == 'exit': sys.exit()    
+
+        match operation:
+            case 'read':
+                operation_path = operation + ' ' + current_directory + '/' + filename
+                client.send(operation_path.encode())
+                read_file_from_server(filename, client)
+
+                if input('Do you want to do more operations? (y/n) \n') == 'y':
+                    client.send(b'<Continue>')#inform server that I want to do more operations
+                    continue
+                break
+            case 'write':
+                operation_path = operation + ' ' + current_directory + '/' + filename
+                client.send(operation_path.encode())
+                success = write_file_to_server(filename, client)
+                if not success:
+                    print('Error! Please try again')
+                    continue
+                directories_dict = receive_tree_from_server(client)
+                print("These are the files currently in the server after writing"  + str(directories_dict) + " directory: ")
+                print(client.recv(1024).decode())#print the server response
+                if input('Do you want to do more operations? (y/n) \n') == 'y':
+                    client.send(b'<Continue>')#inform server that I want to do more operations
+                    continue
+                break
+            case 'update':         
+                operation_path = operation + ' ' + current_directory + '/' + filename       
+                client.send(operation_path.encode())
+                print('The file will now be opened automatically, please make your changes and save the file')
+                read_file_from_server(operation_path, client)
+                while True:
+                    user_done = input('Once you are done, please close the file and enter "y" \n') == 'y'
+                    if user_done:
+                        write_file_to_server(filename, client)
+                        directories_dict = receive_tree_from_server(client)        
+                        print(client.recv(1024).decode())#print the server response
+                        break
+                    else: 
+                        if input('Are you sure you want to exit without updating the file on the server? (y/n) \n') == 'y':
+                            client.send(b'<Cancel>')#inform server that update was cancelled
+                            break
+                
+                if input('Do you want to do more operations? (y/n) \n') == 'y':
+                    client.send(b'<Continue>')#inform server that I want to do more operations
+                    continue
+                break
+            case 'delete':
+                # if check_filename_exists(filename, filenames_in_directory) == False:
+                #     continue
+                operation_path = operation + ' ' + current_directory + '/' + filename
+                client.send(operation_path.encode())
+                directories_dict = receive_tree_from_server(client)
+                print("These are the files currently in the server "  + str(directories_dict) + " directory: ")
+                print(client.recv(1024).decode())#print the server response <Done>   
+                                
+                if input('Do you want to do more operations? (y/n) \n') == 'y':
+                    client.send(b'<Continue>')#inform server that I want to do more operations
+                    continue
+                break
+            case 'cd':
+                #get rid of back steps
+                current_directory, filename = cd_back_steps(current_directory, filename)
+                #if there is a path after stepbacks, add it to the current directory
+                if filename != '':
+                    current_directory += '/' + filename
+                continue
+            case _:#handle if invalid operation
+                print('Invalid input, please try again2')
+                continue
+
+    client.send('<Exit>'.encode())
+    client.close()
+
+
+# file_size = client.recv(1024).decode()
+# print(file_size)
+
+
+            
+
---
+++ utils/server.py
@@ -0,0 +1,170 @@
+import os
+import socket
+import pickle
+
+def send_file(filename, client):
+    try:
+        file = open('sender_folder/'+filename, 'rb')
+        data = file.read(1024)
+        while data:
+            client.send(data)
+            data = file.read(1024)
+        client.send(b'<End>')
+        file.close()
+        return True
+    except ConnectionResetError:#If the client disconnected
+        return False
+
+def receive_write_file(filename, client):
+    try:
+        data = client.recv(1024)
+        if data.endswith(b'<Cancel>'): # If the client cancelled the update operation
+            return True # Return True because client cancelled the operation but didn't disconnect
+        file = open('sender_folder/'+filename, 'wb')
+        while data:
+            if data.endswith(b'<End>'):
+                data = data[:-5]
+                file.write(data)
+                break
+            file.write(data)
+            data = client.recv(1024)
+        file.close()
+        send_tree_to_client(client)
+        client.send(b'<Done>') #Acknowledge the client that the operation is done
+        return True
+    except ConnectionResetError:#If the client disconnected
+        return False
+    
+def build_directory_tree(directory):
+    result = {}
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            result[item] = build_directory_tree(item_path)
+        else:
+            result[item] = None  # You can set it to a specific value or leave it as None
+    return result
+
+def send_tree_to_client(client):
+    tree = build_directory_tree('sender_folder/')
+    tree = pickle.dumps(tree)
+    client.sendall(tree)
+    
+# print('SERVER INITIALIZING...')
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
+# server.bind(('localhost', 9999))
+# server.listen()
+# print('SERVER RUNNING...')
+
+def server_main_app(client):
+        print('==============================================')
+        print('CONNECTED TO A CLIENT...')
+        # client, address = server.accept()
+        # print(f'CLIENT {address} CONNECTED...')
+
+        # connection_opened = True
+
+        try: #First, send the tree of the directory and subdirectories to the client
+            #Send the tree of directories and files on the server
+            send_tree_to_client(client)
+        except ConnectionResetError:#If the client disconnected:
+            print('CLIENT DISCONNECTED')
+            connection_opened = False
+            client.close()
+            return
+
+        subfolder = ''
+        connection_opened = True
+        #SERVE THIS CLIENT WITH AS MANY OPERATIONS AS HE WANTS
+        while connection_opened:
+            
+            
+            try:
+                # #Send the list of files on the server
+                # client.send(str(files_on_server).encode())
+                # #send the list of subfolders on the server
+                # all_subdirectories = [x[0] for x in os.walk('sender_folder/'+subfolder)]
+                operation_path = client.recv(1024).decode() #Receive the operation and filename from the client
+            except ConnectionResetError:#If the client disconnected:
+                print('CLIENT DISCONNECTED')
+                connection_opened = False
+                client.close()
+                break
+            print(operation_path)
+            operation_path_list = operation_path.split(' ', 1)
+            
+            try:
+                operation = operation_path_list[0].lower()
+                #substring path to remove first 5 characters "root/"
+                path = operation_path_list[1][5:]
+                print(path)
+            except:
+                #Close the connection because the client request errors are handled at client. 
+                #If the client sent an invalid request, then it means that the client is not working properly
+                print('CLIENT REQUESTED INVALID OPERATION')
+                connection_opened = False
+                client.close()
+                break
+            
+            print('CLIENT REQUESTED OPERATION: '+operation_path)
+            match operation:
+                case 'read':
+                    success = send_file(path, client)
+                    if not success:
+                        print('CLIENT DISCONNECTED')
+                        connection_opened = False
+                        client.close()
+                        break
+                    print('READ REQUEST SERVED!')
+                case 'write':
+                    print('RECEIVING FILE...')
+                    success = receive_write_file(path, client)
+                    if not success:
+                        print('CLIENT DISCONNECTED')
+                        connection_opened = False
+                        client.close()
+                        break
+                    print('WRITE REQUEST SERVED!')
+                case 'update':
+                    print('SENDING CURRENT FILE STATUS...')
+                    success = send_file(path, client)
+                    if not success:
+                        print('CLIENT DISCONNECTED')
+                        connection_opened = False
+                        client.close()
+                        break
+                    print('FILE STATUS SENT!')
+                    print('RECEIVING UPDATED FILE...')
+                    success = receive_write_file(path, client)
+                    if not success:
+                        print('CLIENT DISCONNECTED')
+                        connection_opened = False
+                        client.close()
+                        break
+                    print('UPDATE REQUEST SERVED!')
+                case 'delete':
+                    try:
+                        os.remove('sender_folder/'+path)
+                    except ConnectionResetError:
+                        send_tree_to_client(client)
+                        client.send(b'<Fail>')
+                    send_tree_to_client(client)
+                    client.send(b'<Done>')
+                    print('DELETE REQUEST SERVED!')
+                case 'create':
+                    pass
+                case 'cd':
+                    pass
+
+            try:
+                if(client.recv(1024).decode() == '<Exit>'):
+                    connection_opened = False
+                    client.close()
+            except ConnectionResetError:
+                print('CLIENT DISCONNECTED')
+                connection_opened = False
+                client.close()
+                break
+        print('CLIENT OPERATION TERMINATED...')
+
+
